4e1c271eac6d4c68e38325e1d67e1b9575b3f651
Saifan Aremenak <saifanaremenak@gmail.com>
2025-06-26 21:08:46 +0200

    Add filter_by_age function to filter users by minimum age

--- filter_users.py
+++ filter_users.py
@@ -10,12 +10,21 @@ def filter_users_by_name(name):
     for user in filtered_users:
         print(user)
 
+def filter_users_by_age(min_age):
+    with open("users.json", "r") as file:
+        users = json.load(file)
+    filtered_user= [user for user in users if user["age"] >= min_age]
 
+    for user in filtered_user:
+        print(user)
 if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? (Currently, only 'name' is supported): ").strip().lower()
+    filter_option = input("What would you like to filter by? (Currently, 'name' and 'age' is supported): ").strip().lower()
 
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
         filter_users_by_name(name_to_search)
+    elif filter_option == "age":
+         min_age = int(input("Enter an age to filter users: ").strip())
+         filter_users_by_age(min_age)
     else:
         print("Filtering by that option is not yet supported.")
